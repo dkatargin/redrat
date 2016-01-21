b92d39c0a54a3c96c6ee3f7099fe56721fa496b8
Dmitry K <dimon90ster@gmail.com>
2016-01-21 18:11:16 +0300

    Editor: add status/owner changers

---
+++ editor.py
@@ -0,0 +1,48 @@
+from connector import redmine
+
+
+class EditIssues:
+    def __init__(self, edit_type, issue_id):
+        self.redmine = redmine()
+        self.issue_id = issue_id
+        if edit_type == 'status':
+            self.change_status()
+        elif edit_type == 'chown':
+            self.change_owner()
+        elif edit_type == 'comment':
+            self.add_comment()
+        else:
+            pass
+
+    def change_status(self):
+        selected_issue = self.redmine.issue.get(self.issue_id)
+        out_str = '\033[1m%s\033[0m\n%s\nСтатус:\033[4m%s\033[0m\n' % (selected_issue,selected_issue.description,selected_issue.status)
+        print(out_str+'='*10)
+        print('Выберете новый статус(цифра) задачи:\n')
+        for i in self.redmine.issue_status.all():
+            print("[%i]\t%s"%(i.id, i))
+        new_status = int(input('>> '))
+        selected_issue.status_id = new_status
+        if selected_issue.save():
+            print('Статус задачи изменён')
+        else:
+            print('Статус задачи не изменён')
+
+    def change_owner(self):
+        selected_issue = self.redmine.issue.get(self.issue_id)
+        issue_proj = list(filter(lambda p: p.name == selected_issue.project.name, self.redmine.project.all()))
+        if not issue_proj:
+            return
+        issue_proj = issue_proj[0]
+        print('На кого перевести задачу(число)?\n')
+        for m in issue_proj.memberships:
+            print("[%i]\t%s" % (m.id, m.user))
+        new_member = int(input('>> '))
+        selected_issue.assigned_to_id = new_member
+        if selected_issue.save():
+            print('Пользователь задачи изменён')
+        else:
+            print('Пользователь задачи не изменён')
+
+    def add_comment(self):
+        pass
--- main.py
+++ main.py
@@ -1,17 +1,42 @@
+import argparse
 import configparser
+import editor
 import viewer
 
 
-def main_start():
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='RedRat: CLI для работы с RedMine')
+    subparsers = parser.add_subparsers()
+    parser_view = subparsers.add_parser('view', help='Просмотр задач')
+    parser_view.add_argument('view_type', help='list - просмотр в виде списка; num - просмотр количества задач')
+    parser_view.set_defaults(func=viewer_settings)
+
+    parser_edit = subparsers.add_parser('edit', help='Изменение задач')
+    parser_edit.add_argument('edit_type', help='status - смена статуса; comment - добавить комментарий; chown - перевести на userid;')
+    parser_edit.add_argument('issue_id', help='номер задачи')
+    parser_edit.set_defaults(func=editor_settings)
+    return parser.parse_args()
+
+
+def viewer_settings(args):
     config_path = 'settings.conf'
     config = configparser.ConfigParser()
     config.read(config_path)
-
     issue_minimal_priority = int(config.get('ReaderSettings', 'MinimalPriority'))
     issue_exclude_projects = tuple(x.strip() for x in config.get('ReaderSettings', 'ExcludeProjects').split(','))
-    viewer.ViewIssues('num', assigned_to='me',
+    viewer.ViewIssues(args.view_type, assigned_to='me',
                       minimal_priority=issue_minimal_priority,
                       exclude_projects=issue_exclude_projects)
+
+
+def editor_settings(args):
+    editor.EditIssues(edit_type=args.edit_type, issue_id=args.issue_id)
+
+
+def main_start():
+    args = parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
